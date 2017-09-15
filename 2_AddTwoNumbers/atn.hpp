0c3ade7433e1a9015b315316e40cfe0f76d08251
jieyaren <jieyaren@gmail.com>
2017-09-15 10:19:36 +0800

    old solutions

---
+++ 2_AddTwoNumbers/atn.hpp
@@ -0,0 +1,19 @@
+#ifndef __LEETCODE__ATN__
+#define __LEETCODE__ATN__
+#define ListNode
+#if 0
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+ };
+#endif
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+    {
+          l1.val  
+    }
+};
+#endif
